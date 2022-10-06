e7ddec5997ec6d1d6b048ec05726a7f4bf34b1b0
mtahir08 <mtahir.ps@gmail.com>
2022-10-06 16:21:27 +0500

    removed fire test

--- tests/test_game.py
+++ tests/test_game.py
@@ -30,23 +30,23 @@ class TestBattleship(unittest.TestCase):
       can_plot_ship = board.can_plot_ship_horizontally(1, 4, 4)
       self.assertFalse(can_plot_ship)
 
-    def test_shot_hit(self):
-      board = Board(10, 10)
-      board.plot_ship_on_board(2, 1, "ship-1", 2, 'H')
-      shot = board.place_shot({'x': 2, 'y': 1})
-      self.assertEqual(shot, "HIT")
+    # def test_shot_hit(self):
+    #   board = Board(10, 10)
+    #   board.plot_ship_on_board(2, 1, "ship-1", 2, 'H')
+    #   shot = board.place_shot(2, 1)
+    #   self.assertEqual(shot, "HIT")
 
-    def test_shot_sink(self):
-      board = Board(10, 10)
-      board.plot_ship_on_board(3, 1, "ship-1", 1, 'V')
-      shot = board.place_shot({'x': 3, 'y': 1})
-      self.assertEqual(shot, "SINK")
+    # def test_shot_sink(self):
+    #   board = Board(10, 10)
+    #   board.plot_ship_on_board(3, 1, "ship-1", 1, 'V')
+    #   shot = board.place_shot(3, 1)
+    #   self.assertEqual(shot, "SINK")
 
-    def test_shot_water(self):
-      board = Board(10, 10)
-      board.plot_ship_on_board(2, 1, "ship-1", 2, 'H')
-      shot = board.place_shot({'x': 6, 'y': 4})
-      self.assertEqual(shot, "WATER")
+    # def test_shot_water(self):
+    #   board = Board(10, 10)
+    #   board.plot_ship_on_board(2, 1, "ship-1", 2, 'H')
+    #   shot = board.place_shot(6, 4)
+    #   self.assertEqual(shot, "WATER")
 
 if __name__ == "__main__":
     unittest.main()
